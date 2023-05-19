a99a0fdbd7623b36260a4dee5df939a1a96c58a6
pchchv <ipchchv@gmail.com>
2023-05-19 16:07:01 +0300

    Init

---
+++ pass.go
@@ -0,0 +1,9 @@
+// The pass package provides a simple
+// password hashing and verification interface,
+// abstracting several password hashing schemes.
+
+// In most cases only the Hash and Verify functions
+// can be used after initialization,
+// using default contexts and reasonable default values.
+
+package pass
