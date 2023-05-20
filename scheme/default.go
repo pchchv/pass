dc71899bfa475975262c717dab5bdafcca845c22
pchchv <ipchchv@gmail.com>
2023-05-20 14:27:31 +0300

    Add a DefaultSchemes var

---
+++ scheme/default.go
@@ -0,0 +1,16 @@
+package scheme
+
+// Default schemes, the most preferred ones first.
+// The first scheme will be used to hash passwords,
+// and any of the schemes can be used to validate existing passwords.
+// The contents of this value may change in future releases.
+//
+// If you want to change this value, set DefaultSchemes to
+// a slice of the scheme.Scheme array of your construction,
+// rather than changing the array that the slice points to.
+//
+// See the UseDefaults function for more information on how the default schema list is defined.
+// The DefaultSchemes value will not change.
+// You need to call UseDefaults to allow your application to switch to newer hash schemes
+// (either set DefaultSchemes manually, or create a custom context with its own set of schemes).
+var DefaultSchemes []Scheme
