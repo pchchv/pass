7cfbbd49b9ce737ca850a3f60a766c574ea10fdd
pchchv <ipchchv@gmail.com>
2023-05-20 15:45:53 +0300

    Implement a SecureCompare func

---
+++ compare.go
@@ -0,0 +1,12 @@
+package pass
+
+import "crypto/subtle"
+
+// Compares two strings (typical password hashes)
+// in safe mode with constant time.
+// Returns true if they are equal.
+func SecureCompare(a, b string) bool {
+	aByte := []byte(a)
+	bByte := []byte(b)
+	return subtle.ConstantTimeCompare(aByte, bByte) == 1
+}
