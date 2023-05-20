ba3f72bc2f39a3283d418b02c9e9fbb4a2854c4f
pchchv <ipchchv@gmail.com>
2023-05-20 14:56:29 +0300

    Implement a parseKeyValue func

--- hash/argon2/raw/argon2.go
+++ hash/argon2/raw/argon2.go
@@ -5,6 +5,8 @@ package raw
 import (
 	"encoding/base64"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -14,6 +16,8 @@ const (
 	RecommendedMemory  uint32 = 32 * 1024 // Current recommended memory for interactive logins
 	RecommendedThreads uint8  = 4         // Current recommended number of threads for interactive logins.
 )
+
+var ErrInvalidKeyValuePair = fmt.Errorf("invalid argon2 key-value pair")
 
 // Wrapper for golang.org/x/crypto/argon2
 // that implements a sensible hashing interface.
@@ -32,4 +36,24 @@ func Argon2(password string, salt []byte, time, memory uint32, threads uint8) st
 	strSalt := base64.RawStdEncoding.EncodeToString(salt)
 
 	return fmt.Sprintf("$argon2i$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, memory, time, threads, strSalt, strHash)
+}
+
+func parseKeyValue(pairs string) (result map[string]uint64, err error) {
+	parameterParts := strings.Split(pairs, ",")
+
+	for _, parameter := range parameterParts {
+		parts := strings.SplitN(parameter, "=", 2)
+		if len(parts) != 2 {
+			return result, ErrInvalidKeyValuePair
+		}
+
+		parsedint, err := strconv.ParseUint(parts[1], 10, 32)
+		if err != nil {
+			return result, err
+		}
+
+		result[parts[0]] = parsedint
+	}
+
+	return
 }
