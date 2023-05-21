c90549c2c979278699f796084ec856c2f1abd9e4
pchchv <ipchchv@gmail.com>
2023-05-22 00:29:08 +0300

    Implement a makeStub method

--- hash/scrypt/scrypt.go
+++ hash/scrypt/scrypt.go
@@ -2,8 +2,26 @@
 // wrapped in the modular crypt format.
 package scrypt
 
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
 type scryptSHA256Crypter struct {
 	nN int
 	r  int
 	p  int
+}
+
+func (c *scryptSHA256Crypter) makeStub() (string, error) {
+	buf := make([]byte, 18)
+	_, err := rand.Read(buf)
+	if err != nil {
+		return "", err
+	}
+
+	salt := base64.StdEncoding.EncodeToString(buf)
+
+	return fmt.Sprintf("$s2$%d$%d$%d$%s", c.nN, c.r, c.p, salt), nil
 }
