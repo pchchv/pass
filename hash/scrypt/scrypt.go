951319226dab9df58229eaa3354f6730fdd72d5c
pchchv <ipchchv@gmail.com>
2023-05-22 00:30:11 +0300

    Implement a hash method

--- hash/scrypt/scrypt.go
+++ hash/scrypt/scrypt.go
@@ -6,6 +6,8 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+
+	"github.com/pchchv/pass/hash/scrypt/raw"
 )
 
 type scryptSHA256Crypter struct {
@@ -24,4 +26,13 @@ func (c *scryptSHA256Crypter) makeStub() (string, error) {
 	salt := base64.StdEncoding.EncodeToString(buf)
 
 	return fmt.Sprintf("$s2$%d$%d$%d$%s", c.nN, c.r, c.p, salt), nil
+}
+
+func (c *scryptSHA256Crypter) hash(password, stub string) (oldHashRaw []byte, newHash string, salt []byte, N, r, p int, err error) {
+	salt, oldHashRaw, N, r, p, err = raw.Parse(stub)
+	if err != nil {
+		return
+	}
+
+	return oldHashRaw, raw.ScryptSHA256(password, salt, N, r, p), salt, N, r, p, nil
 }
