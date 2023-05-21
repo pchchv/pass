e69b32e48e5b4a5158ab789cb59344b78f24a154
pchchv <ipchchv@gmail.com>
2023-05-22 00:24:20 +0300

    Implmenet a Parse func

--- hash/scrypt/raw/scrypt.go
+++ hash/scrypt/raw/scrypt.go
@@ -5,6 +5,8 @@ package raw
 import (
 	"encoding/base64"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"golang.org/x/crypto/scrypt"
 )
@@ -17,6 +19,8 @@ const (
 	// The current recommended p value for interactive logins.
 	Recommendedp = 1
 )
+
+var ErrInvalidStub = fmt.Errorf("invalid scrypt password stub")
 
 // Wrapper for golang.org/x/crypto/scrypt that implements a sensible modular crypt interface.
 //
@@ -36,4 +40,54 @@ func ScryptSHA256(password string, salt []byte, N, r, p int) string {
 	strSalt := base64.StdEncoding.EncodeToString(salt)
 
 	return fmt.Sprintf("$s2$%d$%d$%d$%s$%s", N, r, p, strSalt, strHash)
+}
+
+// Parse parses a scrypt modular hash or stub string.
+// The format is as follows:
+//
+//	$s2$N$r$p$salt$hash    // hash
+//	$s2$N$r$p$salt         // stub
+func Parse(stub string) (salt, hash []byte, N, r, p int, err error) {
+	if len(stub) < 10 || !strings.HasPrefix(stub, "$s2$") {
+		err = ErrInvalidStub
+		return
+	}
+
+	// $s2$  N$r$p$salt-base64$hash-base64
+	parts := strings.Split(stub[4:], "$")
+
+	if len(parts) < 4 {
+		err = ErrInvalidStub
+		return
+	}
+
+	var Ni, ri, pi uint64
+
+	Ni, err = strconv.ParseUint(parts[0], 10, 31)
+	if err != nil {
+		return
+	}
+
+	ri, err = strconv.ParseUint(parts[1], 10, 31)
+	if err != nil {
+		return
+	}
+
+	pi, err = strconv.ParseUint(parts[2], 10, 31)
+	if err != nil {
+		return
+	}
+
+	N, r, p = int(Ni), int(ri), int(pi)
+
+	salt, err = base64.StdEncoding.DecodeString(parts[3])
+	if err != nil {
+		return
+	}
+
+	if len(parts) >= 5 {
+		hash, err = base64.StdEncoding.DecodeString(parts[4])
+	}
+
+	return
 }
