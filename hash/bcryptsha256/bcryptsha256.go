117d79ebf960c53eaaa5f6d79dc82d0c5ca45e27
pchchv <ipchchv@gmail.com>
2023-05-23 10:32:09 +0300

    Implement a mangle and demangle funcs

--- hash/bcryptsha256/bcryptsha256.go
+++ hash/bcryptsha256/bcryptsha256.go
@@ -8,6 +8,8 @@ package bcryptsha256
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
+	"strings"
 
 	"github.com/pchchv/pass/scheme"
 )
@@ -22,4 +24,23 @@ func (s *schemeSHA256) prehash(password string) string {
 	h.Write([]byte(password))
 
 	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func mangle(hash string) string {
+	parts := strings.Split(hash[1:], "$")
+	salt := parts[2][0:22]
+	h := parts[2][22:]
+
+	return "$bcrypt-sha256$" + parts[0] + "," + parts[1] + "$" + salt + "$" + h
+}
+
+func demangle(stub string) string {
+	if strings.HasPrefix(stub, "$bcrypt-sha256$2") {
+		parts := strings.Split(stub[15:], "$")
+		parts0 := strings.Split(parts[0], ",")
+
+		return "$" + parts0[0] + "$" + fmt.Sprintf("%02s", parts0[1]) + "$" + parts[1] + parts[2]
+	} else {
+		return stub
+	}
 }
